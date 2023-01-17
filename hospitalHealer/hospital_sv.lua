5722d6178f84d176b17444edd56aa40056bf5a2b
ahmeddmohamed610 <117838048+ahmeddmohamed610@users.noreply.github.com>
2023-01-17 20:18:06 +0200

    Delete hospitalHealer directory

--- hospitalHealer/hospital_sv.lua
+++
@@ -1,7 +0,0 @@
-RegisterServerEvent('hospital:price')
-AddEventHandler('hospital:price', function()
-  	local price = 500
-	TriggerEvent('es:getPlayerFromId', source, function(user)
-  	user:removeMoney((price))
- 	end)
-end)
